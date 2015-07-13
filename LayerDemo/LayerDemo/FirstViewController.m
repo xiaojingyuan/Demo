58ffed17b512147c796f0d8444fb0b4deb6b0fd3
xiaojingyuan <562894291@qq.com>
2015-07-13 19:24:33 +0800

    园，弹性

--- LayerDemo/LayerDemo/FirstViewController.m
+++ LayerDemo/LayerDemo/FirstViewController.m
@@ -9,14 +9,31 @@
 #import "FirstViewController.h"
 
 @interface FirstViewController ()
-
+{
+    UIView     *_circleView;
+}
 @end
 
 @implementation FirstViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    _circleView = [[UIView alloc]initWithFrame:CGRectMake(200, 200, 100, 100)];
+    _circleView.backgroundColor = [UIColor redColor];
+    _circleView.layer.cornerRadius = 50;
+    _circleView.layer.masksToBounds = YES;
+    [self.view addSubview:_circleView];
+}
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = touches.anyObject;
+    CGPoint location = [touch locationInView:self.view];
+    
+    [UIView animateWithDuration:5.0 delay:2.0 usingSpringWithDamping:0.1 initialSpringVelocity:1.0 options:UIViewAnimationOptionCurveLinear animations:^{
+        _circleView.center = location;
+    } completion:^(BOOL finished) {
+        
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
